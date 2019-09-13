2d99359dd579ef9db4cb4183d6c5e88449a55c37
Foo-x <www.sand.x@gmail.com>
2019-09-13 21:33:40 +0900

    Update to use only specific style of fontawesome

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import "bulma/css/bulma.min.css";
 import "bulma-divider/dist/css/bulma-divider.min.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
+import "@fortawesome/fontawesome-free/css/solid.css";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
 
 import { Elm } from "./elm/src/Main.elm";
 import { register } from "./elmPorts";
