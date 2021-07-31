18745f847ddec6421a9ba5ec8676828be9e6534b
Foo-x <www.sand.x@gmail.com>
2021-07-31 22:14:58 +0900

    feat: show comment cards
    Refs #12

--- src/page_action/components/Main.tsx
+++ src/page_action/components/Main.tsx
@@ -1,23 +1,153 @@
+import { useContext, useState } from "react";
+import { Second2CommentsContext } from "../contexts/AppContext";
+
+const timestampPattern = /(?:\d{1,2}:)?\d{1,2}:\d{2}/g;
+
+const updateTime = (sec: number) => {
+  chrome.tabs.executeScript({
+    code: `document.querySelector('video').currentTime = ${sec}`,
+  });
+};
+
+const uniqueComments = (s2c: Second2Comments): Second2Comments => {
+  const commentSet = new Set<string>();
+  const result = new Map<number, string[]>() as Second2Comments;
+
+  s2c.forEach((comments, sec) => {
+    const newComments = comments.filter((comment) => !commentSet.has(comment));
+    newComments.forEach((comment) => {
+      commentSet.add(comment);
+      result.set(sec, newComments);
+    });
+  });
+
+  return result;
+};
+
+const secToTimeStr = (sec: number): string => {
+  const hours = Math.floor(sec / 3600);
+  const minutes = Math.floor((sec % 3600) / 60);
+  const seconds = sec % 60;
+
+  return hours === 0
+    ? `${minutes}:${("" + seconds).padStart(2, "0")}`
+    : `${hours}:${("" + minutes).padStart(2, "0")}:${("" + seconds).padStart(
+        2,
+        "0"
+      )}`;
+};
+
+const timestampToSeconds = (timestamp: string): number => {
+  const [first, second, third] = timestamp.split(":");
+  const [h, m, s] =
+    third === undefined
+      ? [0, parseInt(first), parseInt(second)]
+      : [parseInt(first), parseInt(second), parseInt(third)];
+  return h * 3600 + m * 60 + s;
+};
+
+const replaceNewline = (text: string): JSX.Element => {
+  return text
+    .split("\n")
+    .map((text_) => <>{text_}</>)
+    .reduce((pre, cur) => (
+      <>
+        {pre}
+        <br />
+        {cur}
+      </>
+    ));
+};
+
+const replaceTimeLink = (comment: string): JSX.Element => {
+  const resultArray: JSX.Element[] = [];
+
+  let lastIndex = 0;
+  let currentMatch: RegExpExecArray | null;
+  while ((currentMatch = timestampPattern.exec(comment)) !== null) {
+    resultArray.push(
+      replaceNewline(comment.slice(lastIndex, currentMatch.index))
+    );
+    const timestamp = currentMatch[0];
+    const seconds = timestampToSeconds(timestamp);
+    resultArray.push(<a onClick={() => updateTime(seconds)}>{timestamp}</a>);
+    lastIndex = timestampPattern.lastIndex;
+  }
+  resultArray.push(replaceNewline(comment.slice(lastIndex)));
+
+  return resultArray.reduce((pre, cur) => (
+    <>
+      {pre}
+      {cur}
+    </>
+  ));
+};
+
+const s2cToCommentCards = (sec: number, comments: string[]): JSX.Element => {
+  return (
+    <div className="card" key={sec}>
+      <header className="card-header has-background-light">
+        <p className="card-header-title">
+          <a onClick={() => updateTime(sec)}>{secToTimeStr(sec)}</a>
+        </p>
+      </header>
+      <div className="card-content">
+        {comments
+          .map((comment) => <div key={comment}>{replaceTimeLink(comment)}</div>)
+          .reduce((pre, cur) => (
+            <>
+              {pre}
+              <div className="is-divider"></div>
+              {cur}
+            </>
+          ))}
+      </div>
+    </div>
+  );
+};
+
 const Main = () => {
+  const s2c = useContext(Second2CommentsContext);
+  const [selectedSeconds, setSelectedSeconds] = useState<"ALL" | number>("ALL");
+
+  const sideMenu = (
+    <aside className="menu column is-4">
+      <ul id="side-menu-list" className="menu-list side-menu-list">
+        <li>
+          <a
+            className={selectedSeconds === "ALL" ? "is-active" : ""}
+            onClick={() => setSelectedSeconds("ALL")}
+          >
+            ALL
+          </a>
+        </li>
+        {Array.from(s2c.keys(), (key) => {
+          const timeStr = secToTimeStr(key);
+          return (
+            <li key={key}>
+              <a
+                className={selectedSeconds === key ? "is-active" : ""}
+                onClick={() => setSelectedSeconds(key)}
+              >
+                {timeStr}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+  const content =
+    selectedSeconds === "ALL"
+      ? Array.from(uniqueComments(s2c).entries()).map(([sec, comments]) =>
+          s2cToCommentCards(sec, comments)
+        )
+      : s2cToCommentCards(selectedSeconds, s2c.get(selectedSeconds)!);
+
   return (
     <main className="columns is-mobile is-gapless main-container" role="main">
-      <aside className="menu column is-4">
-        <ul id="side-menu-list" className="menu-list side-menu-list">
-          <li>
-            <a className="is-active">ALL</a>
-          </li>
-        </ul>
-      </aside>
-      <section className="column is-8">
-        <div className="card">
-          <header className="card-header has-background-light">
-            <p className="card-header-title">1:23</p>
-          </header>
-          <div className="card-content">
-            <div>foo</div>
-          </div>
-        </div>
-      </section>
+      {sideMenu}
+      <section className="column is-8">{content}</section>
     </main>
   );
 };
--- src/page_action/pageAction.tsx
+++ src/page_action/pageAction.tsx
@@ -7,7 +7,10 @@ import "bulma/css/bulma.min.css";
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { MemoryRouter as Router, Route, Switch } from "react-router-dom";
-import { TotalCountContext } from "./contexts/AppContext";
+import {
+  Second2CommentsContext,
+  TotalCountContext,
+} from "./contexts/AppContext";
 import ConfigPage from "./pages/ConfigPage";
 import MainPage from "./pages/MainPage";
 
@@ -15,12 +18,21 @@ type Msg = PageToPA | ViewPropsToPA;
 
 const PageAction = () => {
   const [totalCount, setTotalCount] = useState(0);
+  const [s2c, setS2C] = useState<Second2Comments>(new Map());
 
   useEffect(() => {
     chrome.runtime.onMessage.addListener((msg: Msg) => {
       console.log(msg);
       if (msg.type === "page") {
         setTotalCount(msg.totalCount);
+        setS2C(
+          new Map(
+            Object.entries(msg.data).map(([sec, comments]) => [
+              parseInt(sec),
+              comments,
+            ])
+          )
+        );
       }
     });
 
@@ -32,7 +44,9 @@ const PageAction = () => {
   return (
     <>
       <TotalCountContext.Provider value={totalCount}>
-        <Route exact path="/" component={MainPage} />
+        <Second2CommentsContext.Provider value={s2c}>
+          <Route exact path="/" component={MainPage} />
+        </Second2CommentsContext.Provider>
       </TotalCountContext.Provider>
       <Route exact path="/config" component={ConfigPage} />
     </>
