a6084f0e3ece55a94d27ba9192336f782708e17e
Foo-x <www.sand.x@gmail.com>
2021-07-31 22:50:47 +0900

    feat: style depends on s2c
    Refs #12

--- src/page_action/pages/MainPage.tsx
+++ src/page_action/pages/MainPage.tsx
@@ -1,9 +1,19 @@
+import { useContext } from "react";
 import Main from "../components/Main";
 import MainPageHeader from "../components/MainPageHeader";
+import { Second2CommentsContext } from "../contexts/AppContext";
 
 const MainPage = () => {
+  const s2c = useContext(Second2CommentsContext);
+
   return (
-    <div className="page-action-with-comments">
+    <div
+      className={
+        s2c.size === 0
+          ? "page-action-without-comments"
+          : "page-action-with-comments"
+      }
+    >
       <MainPageHeader />
       <Main />
     </div>
