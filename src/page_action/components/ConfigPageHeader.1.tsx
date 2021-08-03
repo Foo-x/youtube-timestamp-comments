0bf3da500807d1f7991bc322ad7fba242d922f0c
Foo-x <www.sand.x@gmail.com>
2021-08-03 21:30:14 +0900

    style: change cog color on config page

--- src/page_action/components/ConfigPageHeader.1.tsx
+++ src/page_action/components/ConfigPageHeader.1.tsx
@@ -7,7 +7,7 @@ const ConfigPageHeader = () => {
         <div className="config-header-menu navbar-menu is-flex">
           <div className="navbar-end">
             <Link to="/" className="navbar-item">
-              <span className="icon">
+              <span className="icon active">
                 <i className="fas fa-cog fa-sm" />
               </span>
             </Link>
